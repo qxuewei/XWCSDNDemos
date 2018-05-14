172779d763072a8d1dfa6e959e05d94f09c1f9cd
邱学伟 <qxuewei@yeah.net>
2018-05-14 16:18:53 +0800

    线程

---
+++ XWThreadDemo/XWThreadDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XWThreadDemo
+//
+//  Created by 邱学伟 on 2018/5/14.
+//  Copyright © 2018年 邱学伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+
+@end
---
+++ XWThreadDemo/XWThreadDemoTests/XWThreadDemoTests.m
@@ -0,0 +1,57 @@
+//
+//  XWThreadDemoTests.m
+//  XWThreadDemoTests
+//
+//  Created by 邱学伟 on 2018/5/14.
+//  Copyright © 2018年 邱学伟. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XWThreadDemoTests : XCTestCase
+
+@end
+
+@implementation XWThreadDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testNSThread {
+    /// 获取当前线程
+    NSThread *currentThread = [NSThread currentThread];
+    
+    /// 创建需要自己启动的线程
+    NSThread *creatThread = [[NSThread alloc] initWithTarget:self selector:@selector(runMethod) object:nil];
+    [creatThread start];
+
+    /// 创建自动启动的线程
+    [NSThread detachNewThreadSelector:@selector(runMethod2) toTarget:self withObject:nil];
+}
+- (void)runMethod {
+    NSLog(@"runMethod ++ %@",[NSThread currentThread]);
+}
+- (void)runMethod2 {
+    NSLog(@"runMethod2 ++ %@",[NSThread currentThread]);
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
